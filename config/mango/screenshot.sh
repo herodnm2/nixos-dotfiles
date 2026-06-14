b1b47d782c149697b26bba5b89931b66c10dc06a
clevex <223722111+herodnm2@users.noreply.github.com>
2026-06-14 18:53:04 +0300

    Remove config directories

--- config/mango/screenshot.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-grim ~/Pictures/$(date +%Y-%m-%d_%H-%M-%S).png
--- config/waybar/scripts/music.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-STATE_FILE="/tmp/waybar-music-hidden"
-
-if [ -f "$STATE_FILE" ]; then
-    echo ""
-else
-    playerctl metadata --format "{{artist}} - {{title}}" 2>/dev/null
-fi
